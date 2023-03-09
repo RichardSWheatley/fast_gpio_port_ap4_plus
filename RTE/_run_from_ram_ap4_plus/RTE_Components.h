4439cfe301f1bddecfa6be60dcc1e3acad941453
Mariusz Lacina <mariusz.lacina@ambiq.com>
2023-03-09 16:10:13 +0100

    first commit

---
+++ RTE/_run_from_ram_ap4_plus/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'run_from_ram_ap4_plus' 
+ * Target:  'run_from_ram_ap4_plus' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "apollo4p.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
